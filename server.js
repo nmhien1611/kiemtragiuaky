c175a695fac7cc9332f086db035fe3cd3d59d788
nmhien1611 <nmhien1611@gmail.com>
2026-03-12 14:09:40 +0700

    A1: Khoi tao Node.js va Express server

---
+++ server.js
@@ -0,0 +1,15 @@
+JavaScript
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server chạy tại http://localhost:${PORT}`);
+});
